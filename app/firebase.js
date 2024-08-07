663634ddfe153aab1cf4e043965c94390cf9f576
Javaria Shabbir <l216053@lhr.nu.edu.pk>
2024-08-07 17:28:07 +0500

    firebase setup

---
+++ app/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDBzQNRGTc61uO7QbhrUmJMvvNRot0fHUk",
+  authDomain: "pantrytrackerapplication.firebaseapp.com",
+  projectId: "pantrytrackerapplication",
+  storageBucket: "pantrytrackerapplication.appspot.com",
+  messagingSenderId: "608375358748",
+  appId: "1:608375358748:web:1c4c42ba71759b5334e6d9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
