ac1b9a01058ea40999620800bd5f0d695ce6c010
Javaria Shabbir <l216053@lhr.nu.edu.pk>
2024-08-07 17:06:53 +0500

    items added and adjusted

--- app/page.js
+++ app/page.js
@@ -1,17 +1,43 @@
-import Image from "next/image";
+'use client'
+import React ,{useState,useEffect}from "react"; 
+
 
 export default function Home() {
+  const [items,setItems]=useState([
+    {name:'Coffee',price:4.95},
+    {name:'Chocolate',price:4.95},
+    {name:'Candy',price:7.95}
+  ]);
+  const [total,setTotal]=useState(0)
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
        <h1 className="text-4xl p-4 text-center">Pantry Tracker Application</h1>
       
-      <div>
-        <form>
-          <input type="text" placeholder='Enter Item'></input>
-          <input type="number" placeholder='Enter $'></input>
-          <button type="submit">+</button>
+      <div className="bg-slate-800 p-4 rounded-lg">
+        <form className="grid grid-cols-6 items-center text-black">
+          <input 
+          className='col-span-3 p-3 border' type="text" placeholder='Enter Item'>
+          </input>
+          <input 
+          className='col-span-3 p-3 border mx-3' type="number" placeholder='Enter $'>
+          </input>
+          <button 
+          className='text-white bg-slate-950 hover:bg-slate-900 p-3 text-xl' type="submit">
+            +
+          </button>
         </form>
+        <ul>
+          {items.map((item,id)=>(
+             <li key={id} className="my-4 w-fill flex justify-between bg-slate-950">
+              <div className="p-4 w-full flex justify-between"> 
+                <span className="capitalize">{item.name}</span>
+                <span>${item.price}</span>
+              </div>
+              <button className="ml-8 p-4 border-l-2 border-slate-900 hover:bg-slate-900 x-16">X</button>
+             </li>
+          ))}
+        </ul>
       </div>
 
       </div>
