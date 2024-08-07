b9bed5fa0c7bfc445c676b7cc269052275c97a2b
Javaria Shabbir <l216053@lhr.nu.edu.pk>
2024-08-07 16:52:04 +0500

    form added

--- app/page.js
+++ app/page.js
@@ -5,6 +5,15 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
        <h1 className="text-4xl p-4 text-center">Pantry Tracker Application</h1>
+      
+      <div>
+        <form>
+          <input type="text" placeholder='Enter Item'></input>
+          <input type="number" placeholder='Enter $'></input>
+          <button type="submit">+</button>
+        </form>
+      </div>
+
       </div>
     </main>
   );
